b577043a09c2d9a4f54a84f3d2e549c62c9c67eb
Aqstoria <aqstoria@gmail.com>
2025-07-26 06:24:54 -0700

    Update footer: Change copyright to 2025 and replace 'Powered by sustainable hosting' with 'Made with  by Aqstoria Team'

--- app/page.tsx
+++ app/page.tsx
@@ -653,7 +653,7 @@ export default function Portfolio() {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 Aqstoria. All rights reserved. | Powered by sustainable hosting</p>
+            <p>&copy; 2025 Aqstoria. All rights reserved. | Made with ❤️ by Aqstoria Team</p>
           </div>
         </div>
       </footer>
